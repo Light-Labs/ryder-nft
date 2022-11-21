d6fbe6522930eb812cab87fee312b432a44648c1
friedger <mail@friedger.de>
2022-11-21 10:17:41 +0100

    chore: add more tests (WIP)

--- backend-solidity/src/tests/ryder-nft.js
+++ backend-solidity/src/tests/ryder-nft.js
@@ -9,7 +9,7 @@ describe("nf-token", function () {
   beforeEach(async () => {
     [owner, bob, jane, sara] = await ethers.getSigners();
     const ryderNftContract = await ethers.getContractFactory("RyderNFT");
-    const admins = [owner.getAddress()]
+    const admins = [owner.getAddress()];
     ryderNft = await ryderNftContract.deploy(admins);
     await ryderNft.deployed([owner]);
   });
@@ -22,10 +22,23 @@ describe("nf-token", function () {
     expect(await ryderNft.tokenIdToTierId(5003)).to.equal(100);
   });
 
-  /*
   it("throws if dickson parameter is set twice", async function () {
-    await nfToken.connect(owner).setDicksonParameter(100);
-    await expect(nfToken.connect(owner).setDicksonParameter(200)).to.be.revertedWithCustomError;
+    await ryderNft.connect(owner).setDicksonParameter(100)
+    await expect(ryderNft.connect(owner).setDicksonParameter(200)).to.be
+      .revertedWithCustomError;
   });
-  */
+
+  it("return the correct meta data", async function () {
+    expect(await ryderNft.name()).to.equal("Ryder NFT");
+    expect(await ryderNft.symbol()).to.equal("RYD");
+    expect(await ryderNft.tokenURI(1)).to.equal("ipfs://");
+  });
+
+  it('correctly burns a NFT', async function() {
+    await ryderNft.connect(owner).setMinter(owner.getAddress(), true);
+    await ryderNft.connect(owner).mint(bob.address)
+    expect(await ryderNft.connect(bob).burn(id1)).to.emit(ryderNft, 'Transfer');
+    expect(await ryderNft.balanceOf(bob.address)).to.equal(0);
+    await expect(ryderNft.ownerOf(id1)).to.be.revertedWith('003002');
+  });
 });
