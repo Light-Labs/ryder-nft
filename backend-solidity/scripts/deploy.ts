a1eab53c13336645c6c35f4cd47280f9eac21484
Marvin <MarvinJanssen@users.noreply.github.com>
2023-01-08 16:15:33 -0800

    Ryder Mint V2 on Ethereum (#17)
    * feat: ryder-mint-v2 for Eth
    
    * chore: add uncommitted Eth changes
    
    * fix: mistake
    
    * feat: add tests
    
    * chore: add/update ABI
    
    * chore: add deploy script for V2
    
    * fix: div by zero error and add test

--- backend-solidity/scripts/deploy.ts
+++ backend-solidity/scripts/deploy.ts
@@ -1,32 +1,37 @@
 import { ethers } from "hardhat";
 
+const paymentRecipient = "0xF70c34D7D589aae3E346aF5aC435034bC1E64386";
+const admins = [
+  "0x2157D514EE7abFD13f4d7e740CaC2CEEcC0bf4ef",
+  "0x4e82C2935ad9bD63b45aAa24a26dA00a1c2bfa1a",
+  "0x0B65Df3609EBa3E8Ad70fC31C7916746173dA2ED"
+];
+
 async function main() {
   const [deployer] = await ethers.getSigners();
 
-  console.log("Deploying contracts with the account:", deployer.address);
-  console.log("Recipient of Payments", deployer.address);
-
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log("Deploying contracts with the account: ", deployer.address);
+  console.log("Recipient of payments: ", paymentRecipient);
+  // console.log("Account balance:", (await deployer.getBalance()).toString());
 
   const [ryderNftContract, ryderMintContract] = await Promise.all([
     ethers.getContractFactory("RyderNFT"),
-    ethers.getContractFactory("RyderMint"),
+    ethers.getContractFactory("RyderMint")
   ]);
-
   const ryderNft = await ryderNftContract.deploy([deployer.address]);
   const ryderMint = await ryderMintContract.deploy(ryderNft.address, []);
-
-  await ryderMint.setPaymentRecipient(deployer.address, {
-    from: deployer.address,
-  });
+  await ryderMint.setPaymentRecipient(paymentRecipient, { from: deployer.address });
   await ryderNft.setMinter(ryderMint.address, true, { from: deployer.address });
-
-  console.log("Done");
+  for (let admin of admins) {
+    await ryderMint.setAdmin(admin, true, { from: deployer.address });
+    await ryderNft.setAdmin(admin, true, { from: deployer.address });
+  }
+  console.log(`Deployed contracts and did setup\nRyderNFT: ${ryderNft.address}\nRyderMint: ${ryderMint.address}\nAdmins: ${admins.join(', ')}`);
+  console.log(`RyderNFT deploy TXID: ${ryderNft.deployTransaction.hash}`);
+  console.log(`RyderMINT deploy TXID: ${ryderMint.deployTransaction.hash}`);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
