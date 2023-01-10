3ca7ceaad7d5399ea77ed3dbe9eff7dfd9c6b96b
MarvinJanssen <MarvinJanssen@users.noreply.github.com>
2023-01-10 15:04:40 -0800

    hotfix: add mainnet config to hardhat and export new MintV2 ABI

--- backend-solidity/hardhat.config.ts
+++ backend-solidity/hardhat.config.ts
@@ -6,9 +6,8 @@ import "hardhat-abi-exporter";
 import "hardhat-gas-reporter";
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY!;
-const GOERLI_PRIVATE_KEY =
-  process.env.GOERLI_PRIVATE_KEY! ||
-  "1234567890123456789012345678901212345678901234567890123456789012";
+const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY!;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY!;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -17,6 +16,10 @@ const config: HardhatUserConfig = {
     tests: "src/tests",
   },
   networks: {
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [MAINNET_PRIVATE_KEY],
+    },
     goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
       accounts: [GOERLI_PRIVATE_KEY],
